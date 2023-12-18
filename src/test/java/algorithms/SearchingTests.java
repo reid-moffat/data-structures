7d45b97ed0f7e00e6aa56584590b4bba9b0ed001
Reid Moffat <18rem8@queensu.ca>
2023-12-18 14:05:35 -0500

    parse TCs

--- src/test/java/algorithms/SearchingTests.java
+++ src/test/java/algorithms/SearchingTests.java
@@ -7,55 +7,76 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ca.reidmoffat.Searching;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class SearchingTests {
 
-    public record SearchTestCase<T, K>(String name, T input1, K input2, int expected) {}
+    interface SearchAlgo<T, K> {
+        public int run(Integer[] array, Integer target);
+    }
+
+    private record SearchTestCase<T, K>(String name, T array, K target, int expected) {}
 
     private static final ArrayList<SearchTestCase<Integer[], Integer>> testCases = new ArrayList<>();
 
     @BeforeAll
     public static void setupTestCases() throws IOException {
 
-        JSONArray a = (JSONArray) JSONValue.parse(Files.readString(Paths.get("./src/test/java/data/Searching.json")));
+        final HashSet<String> tcKeys = new HashSet<>(Arrays.asList("Name", "Array", "Target", "Expected"));
+        final JSONArray tcFile = (JSONArray) JSONValue.parse(Files.readString(Paths.get("./src/test/java/data/Searching.json")));
 
-        System.out.println("Size: " + a.size() + " First element: " + ((JSONObject) a.get(0)).keySet());
+        for (int i = 0; i < tcFile.size(); ++i) {
+            final JSONObject tc = (JSONObject) tcFile.get(i);
 
-        var tc = new SearchTestCase<>("Empty array & ", new Integer[] { }, 1, -1);
-        testCases.add(tc);
+            if (!tc.keySet().equals(tcKeys)) {
+                throw new IllegalStateException("Test case #" + (i + 1) + " (value: " + tc + ") is invalid; must have " +
+                        "the following keys: " + tcKeys + ", but it has " + tc.keySet());
+            }
 
-        tc = new SearchTestCase<>("Empty array", new Integer[] { }, -1, -1);
-        testCases.add(tc);
+            // Extract data from JSON & create test case with it
+            final String name = (String) tc.get("Name");
+            final int target = Math.toIntExact((Long) tc.get("Target"));
+            final int expected = Math.toIntExact((Long) tc.get("Expected"));
 
-        tc = new SearchTestCase<>("Empty array", new Integer[] { }, 0, -1);
-        testCases.add(tc);
+            final JSONArray jsonArray = (JSONArray) tc.get("Array"); // Need loop to convert JSONArray -> array
+            final Integer[] array = new Integer[jsonArray.size()];
+            for (int j = 0; j < jsonArray.size(); ++j) array[j] = (int) jsonArray.get(j);
+
+            testCases.add(new SearchTestCase<>(name, array, target, expected));
+        }
     }
 
-    @Test
-    public void BinarySearch() {
+    private void runAllTCs(String searchAlgorithm, SearchAlgo<Integer[], Integer> algo) {
 
         System.out.println("\n====================================================");
-        System.out.println("Running all searching test cases for BinarySearch...");
+        System.out.println("Running all searching test cases for " + searchAlgorithm + "...");
         System.out.println("====================================================");
 
         for (int i = 0; i < testCases.size(); ++i) {
             var testCase = testCases.get(i);
             System.out.println("\nRunning TC #" + i + ": " + testCase.name());
+            System.out.println("Array: " + Arrays.toString(testCase.array));
+            System.out.println("Target value: " + testCase.target);
+            System.out.println("Expected position (-1 = not found): " + testCase.expected);
 
-            int result = Searching.binarySearch(testCase.input1(), testCase.input2());
+            int result = algo.run(testCase.array, testCase.target);
 
             if (testCase.expected() != result) {
-                String message = "Expected " + testCase.input2() + " to be in position " + testCase.expected() +
-                        " in array " + Arrays.toString(testCase.input1()) + ", but BinarySearch() returned " + result;
+                String message = "Expected " + testCase.target + " to be in position " + testCase.expected() + " in array "
+                        + Arrays.toString(testCase.array) + ", but " + searchAlgorithm + "() returned " + result;
                 throw new AssertionError(message);
             }
             System.out.println("Test case passed successfully");
         }
+    }
+
+    @Test
+    public void BinarySearch() {
+        runAllTCs("BinarySearch", Searching::binarySearch);
     }
 }
